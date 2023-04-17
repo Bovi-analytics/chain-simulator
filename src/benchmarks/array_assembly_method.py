06a1d27b6894d92f3daf541e85bdfa7d39b94dcd
Maxximiser <mjh.nollet@student.han.nl>
2023-04-17 07:59:27 +0200

    Added benchmarks to assemble a COO, CSC and a CSR array

--- src/benchmarks/array_assembly_method.py
+++ src/benchmarks/array_assembly_method.py
@@ -1,76 +1,164 @@
 from textwrap import dedent
 from timeit import Timer
+from typing import Callable, Iterable, Optional, TypeVar
+
+import numpy as np
+
+from benchmarks.abstract import csv_writer
+from benchmarks.tools import scipy_coo_array_info, scipy_cs_array_info
 
 
-def iterate_lil():
-    setup = dedent("""
+def array_setup(array_type: str, axis_size: int, cells_to_fill: int) -> str:
+    return dedent(
+        f"""
+        from math import pow
+
+        import numpy as np
+        from scipy.sparse import {array_type:s}
+        
+        rng = np.random.default_rng(1)
+    
+        axis_size = {axis_size:d}
+        cells_to_fill = {cells_to_fill:d}
+        if cells_to_fill:
+            array_size = int(pow(axis_size, 2))
+            cells = rng.choice(array_size, size=cells_to_fill, replace=False)
+            rows, cols = np.divmod(cells, axis_size)
+            data = rng.random(cells_to_fill)
+        else:
+            rows, cols, data = [], [], []
+        """
+    )
+
+
+_T = TypeVar("_T")
+
+
+def analyze_array(stmt: str, setup: str, extractor: Callable):
+    exec(setup)
+    array = eval(stmt)
+    return extractor(array)
+
+
+def name_timings(timings: Iterable[_T]) -> dict[str, _T]:
+    return {f"run_{run:02d}": timing for run, timing in enumerate(timings)}
+
+
+def run_benchmark(
+        stmt: str, setup: str, number: Optional[int] = None, repeats: int = 10
+) -> dict[str, float]:
+    timer = Timer(stmt, setup)
+    if not number:
+        autorange = timer.autorange()
+        number = autorange[0] if autorange[0] > 2 else 2
+    timings = timer.repeat(repeats, number)
+    return {
+        "number_of_executions": number,
+        "number_of_repeats": repeats,
+        **name_timings(timings)
+    }
+
+
+def benchmark_array_construction() -> None:
+    # Parameter configurations. 16_384
+    range_axis = np.arange(1_024, 16_384 + 1, 1024)
+    range_density = np.arange(0, 1.05, 0.05)
+    # Process parameter configurations for benchmarks.
+    repeat_axes = np.repeat(range_axis, len(range_density))
+    array_sizes = repeat_axes ** 2
+    cells_to_fill = array_sizes * np.tile(range_density, len(range_axis))
+    parameters = tuple(zip(repeat_axes, np.floor(cells_to_fill).astype(int)))
+    # Run benchmarks.
+    construct_coo_init(*parameters)
+    construct_csc_init(*parameters)
+    construct_csr_init(*parameters)
+
+
+def iterate_lil() -> None:
+    setup = dedent(
+        """
     from numpy import array
     from numpy.random import default_rng
     from math import pow
     from scipy.sparse import lil_array
 
-    axis_size = 1_024
+    axis_size = {axis_size:d}
     axis_range = array(range(0, axis_size))
-    cells_count = int(pow(axis_size, 2) * 0.5)
+    cells_count = int(pow(axis_size, 2) * {density:f})
     rows = default_rng(1).choice(axis_range, cells_count)
     cols = default_rng(1).choice(axis_range, cells_count)
     data = default_rng(1).random(cells_count)
-    """)
-    stmt = dedent("""
-    array = lil_array((axis_size, axis_size), dtype="float64")
-    for index_row, index_col, num in zip(rows, cols, data):
-        array[index_row, index_col] = num
-    """)
+    """
+    )
+    stmt = dedent(
+        """
+        array = lil_array((axis_size, axis_size), dtype="float64")
+        for index_row, index_col, num in zip(rows, cols, data):
+            array[index_row, index_col] = num
+        """
+    )
     timer = Timer(stmt, setup)
     data = timer.repeat(10, 10)
     print(data)
 
 
-def construct_csr():
-    setup = dedent("""
-        from numpy import array
-        from numpy.random import default_rng
-        from math import pow
-        from scipy.sparse import csr_array
-
-        axis_size = 3_072
-        axis_range = array(range(0, axis_size))
-        cells_count = int(pow(axis_size, 2) * 0.5)
-        rows = default_rng(1).choice(axis_range, cells_count)
-        cols = default_rng(1).choice(axis_range, cells_count)
-        data = default_rng(1).random(cells_count)
-        """)
-    stmt = dedent("""
-        csr_array((data, (rows, cols)), shape=(axis_size, axis_size))
-        """)
-    timer = Timer(stmt, setup)
-    data = timer.repeat(10, 10)
-    print(data)
+def construct_csr_init(*args: tuple[int, int]) -> None:
+    stmt = "csr_array((data, (rows, cols)), shape=(axis_size, axis_size))"
+    benchmark_data = []
+    for axis_size, cells_to_fill in args:
+        setup = array_setup("csr_array", axis_size, cells_to_fill)
+        array_info = analyze_array(stmt, setup, scipy_cs_array_info)
+        timings = run_benchmark(stmt, setup, repeats=10)
+        benchmark_data.append(
+            {
+                **array_info,
+                "axis_size": axis_size,
+                "cells_to_fill": cells_to_fill,
+                **timings,
+            }
+        )
+    csv_writer("construct_csr_init", benchmark_data)
 
 
-def construct_dok():
-    setup = dedent("""
-        from numpy import array
-        from numpy.random import default_rng
-        from math import pow
-        from scipy.sparse import coo_array
+def construct_csc_init(*args: tuple[int, int]) -> None:
+    stmt = "csc_array((data, (rows, cols)), shape=(axis_size, axis_size))"
+    benchmark_data = []
+    for axis_size, cells_to_fill in args:
+        setup = array_setup("csc_array", axis_size, cells_to_fill)
+        array_info = analyze_array(stmt, setup, scipy_cs_array_info)
+        timings = run_benchmark(stmt, setup, repeats=10)
+        benchmark_data.append(
+            {
+                **array_info,
+                "axis_size": axis_size,
+                "cells_to_fill": cells_to_fill,
+                **timings,
+            }
+        )
+    csv_writer("construct_csc_init", benchmark_data)
 
-        axis_size = 3_072
-        axis_range = array(range(0, axis_size))
-        cells_count = int(pow(axis_size, 2) * 0.5)
-        rows = default_rng(1).choice(axis_range, cells_count)
-        cols = default_rng(1).choice(axis_range, cells_count)
-        data = default_rng(1).random(cells_count)
-        """)
-    stmt = dedent("""
-        coo_array((data, (rows, cols)), shape=(axis_size, axis_size))
-    """)
-    timer = Timer(stmt, setup)
-    data = timer.repeat(10, 10)
-    print(data)
+
+def construct_coo_init(*args: tuple[int, int]) -> None:
+    stmt = "coo_array((data, (rows, cols)), shape=(axis_size, axis_size))"
+    benchmark_data = []
+    for axis_size, cells_to_fill in args:
+        setup = array_setup("coo_array", axis_size, cells_to_fill)
+        array_info = analyze_array(stmt, setup, scipy_coo_array_info)
+        timings = run_benchmark(
+            stmt,
+            setup,
+            repeats=10,
+        )
+        benchmark_data.append(
+            {
+                **array_info,
+                "axis_size": axis_size,
+                "cells_to_fill": cells_to_fill,
+                **timings,
+            }
+        )
+    csv_writer("construct_coo_init", benchmark_data)
 
 
 if __name__ == "__main__":
-    # iterate_lil()
-    construct_csr()
-    construct_dok()
+    benchmark_array_construction()
---
+++ src/benchmarks/tools.py
@@ -0,0 +1,102 @@
+from typing import Any, Iterable, TypeVar, TypedDict
+
+from numpy import dtype, ndarray
+from scipy.sparse import coo_array, csc_array, csc_matrix, csr_array, \
+    csr_matrix
+
+
+class NumpyArrayDict(TypedDict):
+    shape: Iterable[int]
+    size: int
+    ndim: int
+    data_nbytes: int
+    data_dtype: dtype[Any]
+    data_itemsize: int
+
+
+class ScipySparseArrayDict(NumpyArrayDict):
+    nnz: int
+    format: str
+
+
+class ScipyCSArrayDict(ScipySparseArrayDict):
+    indices_nbytes: int
+    indices_dtype: dtype[Any]
+    indices_itemsize: int
+    indptr_nbytes: int
+    indptr_dtype: dtype[Any]
+    indptr_itemsize: int
+    has_sorted_indices: bool
+
+
+class ScipyCOOArrayDict(ScipySparseArrayDict):
+    row_nbytes: int
+    row_dtype: dtype[Any]
+    row_itemsize: int
+    col_nbytes: int
+    col_dtype: dtype[Any]
+    col_itemsize: int
+
+
+def numpy_array_info(array: ndarray) -> NumpyArrayDict:
+    return {
+        "shape": array.shape,
+        "size": array.size,
+        "ndim": array.ndim,
+        "data_nbytes": array.data.nbytes,
+        "data_dtype": array.data.dtype,
+        "data_itemsize": array.data.itemsize,
+    }
+
+
+_CSLIKE = TypeVar("_CSLIKE", csc_array, csc_matrix, csr_array, csr_matrix)
+
+
+def scipy_cs_array_info(array: _CSLIKE) -> ScipyCSArrayDict:
+    """Return information of a CSC or CSR array/matrix as a dictionary.
+
+        Function which maps various properties of a Compressed Sparse Column
+        (CSC) or Compressed Sparse Row (CSR) array/matrix to a dictionary.
+        Useful when writing this data to a CSV-file.
+
+        :param array: CSC or CSR array/matrix to map to a dictionary.
+        :type array: _CSLIKE
+        :return: Dictionary with information about the array/matrix.
+        :rtype ScipyCSArrayDict
+        """
+    return {
+        "shape": array.shape,
+        "size": array.size,
+        "ndim": array.ndim,
+        "data_nbytes": array.data.nbytes,
+        "data_dtype": array.data.dtype,
+        "data_itemsize": array.data.itemsize,
+        "nnz": array.nnz,
+        "format": array.format,
+        "indices_nbytes": array.indices.nbytes,
+        "indices_dtype": array.indices.dtype,
+        "indices_itemsize": array.indices.itemsize,
+        "indptr_nbytes": array.indptr.nbytes,
+        "indptr_dtype": array.indptr.dtype,
+        "indptr_itemsize": array.indptr.itemsize,
+        "has_sorted_indices": array.has_sorted_indices,
+    }
+
+
+def scipy_coo_array_info(array: coo_array) -> ScipyCOOArrayDict:
+    return {
+        "shape": array.shape,
+        "size": array.size,
+        "ndim": array.ndim,
+        "data_nbytes": array.data.nbytes,
+        "data_dtype": array.data.dtype,
+        "data_itemsize": array.data.itemsize,
+        "nnz": array.nnz,
+        "format": array.format,
+        "row_nbytes": array.row.nbytes,
+        "row_dtype": array.row.dtype,
+        "row_itemsize": array.row.itemsize,
+        "col_nbytes": array.col.nbytes,
+        "col_dtype": array.col.dtype,
+        "col_itemsize": array.col.itemsize,
+    }
