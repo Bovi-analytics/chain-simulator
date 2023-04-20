f55d4d4d5243ae579d8022eb404a7a1596bc27ef
Maxximiser <mjh.nollet@student.han.nl>
2023-04-20 19:27:05 +0200

    Renamed fixtures (#3)

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -8,14 +8,14 @@ from chain_simulator.utilities import (
     validate_matrix_sum,
 )
 from pytest import fixture
-from scipy.sparse import csr_array
+from scipy.sparse import coo_array, csr_array
 from typing_extensions import Self
 
 TestingArray = npt.NDArray[np.int32]
 
 
 @fixture  # type: ignore[misc]
-def valid_numpy_ndarray() -> TestingArray:
+def numpy_ndarray_valid() -> TestingArray:
     """Generate a valid NumPy ndarray.
 
     :return: A valid NumPy ndarray.
@@ -25,7 +25,7 @@ def valid_numpy_ndarray() -> TestingArray:
 
 
 @fixture  # type: ignore[misc]
-def invalid_zero_numpy_ndarray() -> TestingArray:
+def numpy_ndarray_zero() -> TestingArray:
     """Generate an invalid NumPy ndarray with only zeroes.
 
     :return: An invalid NumPy ndarray with zeroes.
@@ -35,7 +35,7 @@ def invalid_zero_numpy_ndarray() -> TestingArray:
 
 
 @fixture  # type: ignore[misc]
-def invalid_negative_numpy_ndarray() -> TestingArray:
+def numpy_ndarray_negative() -> TestingArray:
     """Generate an invalid NumPy ndarray with negative numbers.
 
     :return: an invalid NumPy ndarray with negative numbers.
@@ -45,63 +45,55 @@ def invalid_negative_numpy_ndarray() -> TestingArray:
 
 
 @fixture  # type: ignore[misc]
-def valid_scipy_csr_array(valid_numpy_ndarray: TestingArray) -> csr_array:
+def scipy_csr_array_valid(numpy_ndarray_valid) -> csr_array:
     """Generate a valid SciPy csr_array.
 
-    :param valid_numpy_ndarray: Valid array.
-    :type valid_numpy_ndarray: TestingArray.
+    :param numpy_ndarray_valid: Valid array.
+    :type numpy_ndarray_valid: TestingArray.
     :return Valid SciPy csr_array.
     :rtype: csr_array
     """
-    return csr_array(valid_numpy_ndarray)
+    return csr_array(numpy_ndarray_valid)
 
 
 @fixture  # type: ignore[misc]
-def invalid_zero_scipy_csr_array(
-    invalid_zero_numpy_ndarray: TestingArray,
-) -> csr_array:
+def scipy_csr_array_zero(numpy_ndarray_zero) -> csr_array:
     """Generate an invalid SciPy csr_array with only zeroes.
 
-    :param invalid_zero_numpy_ndarray: Array with zeroes.
-    :type invalid_zero_numpy_ndarray: TestingArray
+    :param numpy_ndarray_zero: Array with zeroes.
+    :type numpy_ndarray_zero: TestingArray
     :return: Invalid SciPy csr_array with only zeroes
     :rtype: csr_array
     """
-    return csr_array(invalid_zero_numpy_ndarray)
+    return csr_array(numpy_ndarray_zero)
 
 
 @fixture  # type: ignore[misc]
-def invalid_negative_scipy_csr_array(
-    invalid_negative_numpy_ndarray: TestingArray,
-) -> csr_array:
+def scipy_csr_array_negative(numpy_ndarray_negative) -> csr_array:
     """Generate an invalid SciPy csr_array with negative numbers.
 
-    :param invalid_negative_numpy_ndarray: Array with negative numbers.
-    :type invalid_negative_numpy_ndarray: TestingArray
+    :param numpy_ndarray_negative: Array with negative numbers.
+    :type numpy_ndarray_negative: TestingArray
     :return: Invalid SciPy csr_array with negative numbers.
     :rtype: csr_array
     """
-    return csr_array(invalid_negative_numpy_ndarray)
+    return csr_array(numpy_ndarray_negative)
 
 
 class TestTransitionMatrixSumNumPyNDArray:
     """Test for :func:`~validate_matrix_sum` using NumPy ndarray."""
 
-    def test_sum_to_one(self: Self, valid_numpy_ndarray: TestingArray) -> None:
+    def test_sum_to_one(self: Self, numpy_ndarray_valid) -> None:
         """Test if all rows sum to exactly one."""
-        assert validate_matrix_sum(valid_numpy_ndarray)
+        assert validate_matrix_sum(numpy_ndarray_valid)
 
-    def test_all_zero(
-        self: Self, invalid_zero_numpy_ndarray: TestingArray
-    ) -> None:
+    def test_all_zero(self: Self, numpy_ndarray_zero) -> None:
         """Test when all rows sum to exactly zero."""
-        assert not validate_matrix_sum(invalid_zero_numpy_ndarray)
+        assert not validate_matrix_sum(numpy_ndarray_zero)
 
-    def test_negative(
-        self: Self, invalid_negative_numpy_ndarray: TestingArray
-    ) -> None:
+    def test_negative(self: Self, numpy_ndarray_negative) -> None:
         """Test when all rows sum to exactly one but with negative numbers."""
-        assert not validate_matrix_sum(invalid_negative_numpy_ndarray)
+        assert not validate_matrix_sum(numpy_ndarray_negative)
 
     def test_logging_message(self: Self, caplog: LogCaptureFixture) -> None:
         """Test whether a warning message is emitted."""
@@ -116,21 +108,17 @@ class TestTransitionMatrixSumNumPyNDArray:
 class TestTransitionMatrixSumSciPyCSRArray:
     """Test for :func:`~validate_matrix_sum` using SciPy csr_array."""
 
-    def test_sum_to_one(self: Self, valid_scipy_csr_array: csr_array) -> None:
+    def test_sum_to_one(self: Self, scipy_csr_array_valid) -> None:
         """Test if all rows sum to exactly one."""
-        assert validate_matrix_sum(valid_scipy_csr_array)
+        assert validate_matrix_sum(scipy_csr_array_valid)
 
-    def test_all_zero(
-        self: Self, invalid_zero_scipy_csr_array: csr_array
-    ) -> None:
+    def test_all_zero(self: Self, scipy_csr_array_zero) -> None:
         """Test when all rows sum to exactly zero."""
-        assert not validate_matrix_sum(invalid_zero_scipy_csr_array)
+        assert not validate_matrix_sum(scipy_csr_array_zero)
 
-    def test_negative(
-        self: Self, invalid_negative_scipy_csr_array: csr_array
-    ) -> None:
+    def test_negative(self: Self, scipy_csr_array_negative) -> None:
         """Test when all rows sum to exactly one but with negative numbers."""
-        assert not validate_matrix_sum(invalid_negative_scipy_csr_array)
+        assert not validate_matrix_sum(scipy_csr_array_negative)
 
 
 class TestTransitionMatrixPositive:
