07f2fbc07108a758eb3400f9ab80e0ff7c2392a8
Maxximiser <mjh.nollet@student.han.nl>
2023-05-15 10:32:26 +0200

    Corrected generator chain_simulator to no longer progress one step too far (#19)

--- src/chain_simulator/_simulation.py
+++ src/chain_simulator/_simulation.py
@@ -78,12 +78,12 @@ def chain_simulator(
     Progress a Markov chain by `steps` units of time. Each yield will contain
     two values: the progressed transition matrix and the current step in time.
     The final progressed transition matrix will always be returned, regardless
-    of `interval`. If `interval` is specified, intermediate transition matrices
-    will also be returned.
+    of `interval`. If `interval` is specified, every n-th intermediate
+    transition matrix will also be yielded.
 
     Parameters
     ----------
-    transition_matrix : NumPy 2d-array or SciPy csc/csr matrix/array
+    transition_matrix : NumPy 2d-array or SciPy/CuPy csc/csr matrix/array
         A Markov chain transition matrix.
     steps : int
         How many steps in time the transition matrix must progress in time.
@@ -106,6 +106,12 @@ def chain_simulator(
 
     Notes
     -----
+    Transition matrices are effectively raised to the power of `steps`. This
+    means that :math:`\mathtt{transition\_matrix}^1` yields the same transition
+    matrix as the one given to this generator. This is valid as this initial
+    transition matrix already describes probabilities to transition to the next
+    step in time.
+
     The function is implemented as a generator because of the need of
     intermediary transition matrices. Also, each yield contains the current
     step in time to indicate how far the transition matrix is progressed.
@@ -121,17 +127,17 @@ def chain_simulator(
     ... )
     >>> simulator = chain_simulator(transition_matrix, 3)
     >>> next(simulator)
-    (array([[0, 1 / 8, 7 / 8], [0, 1 / 16, 15 / 16], [0, 0, 1]]), 3)
+    (array([[0, 1 / 4, 3 / 4], [0, 1 / 8, 7 / 8], [0, 0, 1]]), 3)
 
     To get all intermediary results, we can use the parameter `interval`:
 
     >>> simulator = chain_simulator(transition_matrix, 3, interval=1)
     >>> next(simulator)
-    (array([[0, 1 / 2, 1 / 2], [0, 1 / 4, 3 / 4], [0, 0, 1]]), 1)
+    (array([[0, 1, 0], [0, 1 / 2, 1 / 2], [0, 0, 1]]), 1)
     >>> next(simulator)
-    (array([[0, 1 / 4, 3 / 4], [0, 1 / 8, 7 / 8], [0, 0, 1]]), 2)
+    (array([[0, 1 / 2, 1 / 2], [0, 1 / 4, 3 / 4], [0, 0, 1]]), 2)
     >>> next(simulator)
-    (array([[0, 1 / 8, 7 / 8], [0, 1 / 16, 15 / 16], [0, 0, 1]]), 3)
+    (array([[0, 1 / 4, 3 / 4], [0, 1 / 8, 7 / 8], [0, 0, 1]]), 3)
     """
     # Validate `steps` and `interval` parameters for negative values.
     if steps <= 0:
@@ -142,12 +148,15 @@ def chain_simulator(
         )
 
     progressed_matrix = transition_matrix.copy()
-    step_range = range(1, steps + 1)
+    if interval == 1:
+        yield progressed_matrix, 1
+    step_range = range(2, steps + 1)
     for step in step_range:
         progressed_matrix = progressed_matrix.dot(transition_matrix)
         if interval and step < step_range[-1] and step % interval == 0:
             yield progressed_matrix, step
-    yield progressed_matrix, step_range[-1]
+    last_step = step_range[-1] if step_range else 1
+    yield progressed_matrix, last_step
 
 
 def vector_processor_numpy(
--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -42,9 +42,12 @@ def numpy_array() -> NDArray[np.float32]:
     return np.array([[0.0, 1.0, 0.0], [0.0, 0.5, 0.5], [0.0, 0.0, 1.0]])
 
 
-class TestNumPy:
+class TestChainSimulator:
     """Tests for :func:`~chain_simulator` everything related to NumPy."""
 
+    initial_matrix = np.array([[0, 1, 0], [0, 1 / 2, 1 / 2], [0, 0, 1]])
+
+    multiply_zero = np.array([[0, 1, 0], [0, 1 / 2, 1 / 2], [0, 0, 1]])
     multiply_one = np.array([[0, 1 / 2, 1 / 2], [0, 1 / 4, 3 / 4], [0, 0, 1]])
     multiply_two = np.array([[0, 1 / 4, 3 / 4], [0, 1 / 8, 7 / 8], [0, 0, 1]])
     multiply_three = np.array(
@@ -56,102 +59,235 @@ class TestNumPy:
     multiply_five = np.array(
         [[0, 1 / 32, 31 / 32], [0, 1 / 64, 63 / 64], [0, 0, 1]]
     )
+    multiply_six = np.array(
+        [[0, 1 / 64, 63 / 64], [0, 1 / 128, 127 / 128], [0, 0, 1]]
+    )
+    multiply_seven = np.array(
+        [[0, 1 / 128, 127 / 128], [0, 1 / 256, 255 / 256], [0, 0, 1]]
+    )
+    multiply_eight = np.array(
+        [[0, 1 / 256, 255 / 256], [0, 1 / 512, 511 / 512], [0, 0, 1]]
+    )
+    multiply_nine = np.array(
+        [[0, 1 / 512, 511 / 512], [0, 1 / 1024, 1023 / 1024], [0, 0, 1]]
+    )
 
-    def test_multiply_negative_five(self, numpy_array) -> None:
-        """Test `step` parameter of -5."""
+    @pytest.mark.parametrize(
+        "matrix, steps",
+        (
+            pytest.param(initial_matrix, -10, id="steps=-10"),
+            pytest.param(initial_matrix, -5, id="steps=-5"),
+            pytest.param(initial_matrix, 0, id="steps=0"),
+        ),
+    )
+    def test_fail_steps_below_1(self, matrix, steps):
+        """Test `step` parameter with numbers below 1."""
         with pytest.raises(ValueError) as exc_info:
-            next(chain_simulator(numpy_array, -5))
+            next(chain_simulator(matrix, steps))
         assert "higher than 0" in exc_info.value.args[0]
 
-    def test_multiply_zero(self, numpy_array) -> None:
-        """Test `step` parameter of 0."""
-        with pytest.raises(ValueError) as exc_info:
-            next(chain_simulator(numpy_array, 0))
-        assert "higher than 0" in exc_info.value.args[0]
-
-    def test_multiply_one(self, numpy_array) -> None:
-        """Test `step` parameter of 1."""
-        result = next(chain_simulator(numpy_array, 1))
-        assert np.all(result[0] == self.multiply_one)
-        assert result[1] == 1
-
-    def test_multiply_two(self, numpy_array) -> None:
-        """Test `step` parameter of 2."""
-        result = next(chain_simulator(numpy_array, 2))
-        assert np.all(result[0] == self.multiply_two)
-        assert result[1] == 2
-
-    def test_multiply_five(self, numpy_array) -> None:
-        """Test `step` parameter of 5."""
-        result = next(chain_simulator(numpy_array, 5))
-        assert np.all(result[0] == self.multiply_five)
-        assert result[1] == 5
-
-    def test_intermediary_negative(self, numpy_array) -> None:
-        """Test `interval` parameter of -1."""
+    @pytest.mark.parametrize(
+        "matrix, steps, interval",
+        (
+            pytest.param(initial_matrix, 1, -10, id="interval=-10"),
+            pytest.param(initial_matrix, 1, -5, id="interval=-5"),
+            pytest.param(initial_matrix, 1, -1, id="interval=-1"),
+        ),
+    )
+    def test_fail_interval_below_1(self, matrix, steps, interval):
+        """Test `interval` parameter with values below 0."""
         with pytest.raises(ValueError):
             tuple(chain_simulator(numpy_array, 2, -1))
 
-    def test_intermediary_none(self, numpy_array) -> None:
-        """Test `interval` parameter of 0."""
-        results = tuple(chain_simulator(numpy_array, 3, 0))
-        expected = ((self.multiply_three, 3),)
-        assert len(results) == 1
-        assert np.all(results[0][0] == expected[0][0])
-        assert results[0][1] == expected[0][1]
+    @pytest.mark.parametrize(
+        "matrix, steps, matrix_expected",
+        (
+            pytest.param(initial_matrix, 1, multiply_zero, id="steps=1"),
+            pytest.param(initial_matrix, 2, multiply_one, id="steps=2"),
+            pytest.param(initial_matrix, 5, multiply_four, id="steps=5"),
+        ),
+    )
+    def test_correct_output_matrix(self, matrix, steps, matrix_expected):
+        """Test whether a correct final transition matrix is returned."""
+        result = next(chain_simulator(matrix, steps))
+        assert np.all(result[0] == matrix_expected)
 
-    def test_intermediary_all(self, numpy_array) -> None:
-        """Test `interval` parameter of 1."""
-        arrays_actual, steps_actual = zip(*chain_simulator(numpy_array, 3, 1))
-        arrays_expected, steps_expected = zip(
-            *(
-                (self.multiply_one, 1),
-                (self.multiply_two, 2),
-                (self.multiply_three, 3),
-            )
-        )
-        comparisons = [
-            np.any(actual == expected)
-            for actual, expected in zip_longest(arrays_actual, arrays_expected)
-        ]
-        assert len(arrays_actual) == 3
-        assert all(comparisons)
-        assert steps_actual == steps_expected
+    @pytest.mark.parametrize(
+        "matrix, steps, step_expected",
+        (
+            pytest.param(initial_matrix, 1, 1, id="steps=1"),
+            pytest.param(initial_matrix, 2, 2, id="steps=2"),
+            pytest.param(initial_matrix, 5, 5, id="steps=5"),
+        ),
+    )
+    def test_correct_output_step(self, matrix, steps, step_expected):
+        """Test whether a correct final step is returned."""
+        result = next(chain_simulator(matrix, steps))
+        assert result[1] == step_expected
 
-    def test_intermediary_every_second(self, numpy_array) -> None:
-        """Test `interval` parameter of 2."""
-        arrays_actual, steps_actual = zip(*chain_simulator(numpy_array, 5, 2))
-        arrays_expected, steps_expected = zip(
-            *(
-                (self.multiply_two, 2),
-                (self.multiply_four, 4),
-                (self.multiply_five, 5),
-            )
+    @pytest.mark.parametrize(
+        "matrix, steps, interval, steps_expected",
+        (
+            pytest.param(initial_matrix, 10, 0, (10,), id="interval=0"),
+            pytest.param(
+                initial_matrix,
+                10,
+                1,
+                (1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                id="interval=1",
+            ),
+            pytest.param(
+                initial_matrix, 10, 2, (2, 4, 6, 8, 10), id="interval=2"
+            ),
+            pytest.param(
+                initial_matrix, 10, 3, (3, 6, 9, 10), id="interval=3"
+            ),
+            pytest.param(initial_matrix, 10, 4, (4, 8, 10), id="interval=4"),
+            pytest.param(initial_matrix, 10, 5, (5, 10), id="interval=5"),
+            pytest.param(initial_matrix, 10, 6, (6, 10), id="interval=6"),
+            pytest.param(initial_matrix, 10, 7, (7, 10), id="interval=7"),
+            pytest.param(initial_matrix, 10, 8, (8, 10), id="interval=8"),
+            pytest.param(initial_matrix, 10, 9, (9, 10), id="interval=9"),
+            pytest.param(initial_matrix, 10, 10, (10,), id="interval=10"),
+            pytest.param(initial_matrix, 10, 11, (10,), id="interval=11"),
+        ),
+    )
+    def test_intermediary_steps(self, matrix, steps, interval, steps_expected):
+        """Test whether intermediary steps are correct."""
+        _, steps_returned = zip(
+            *tuple(chain_simulator(matrix, steps, interval))
         )
-        comparisons = [
-            np.any(actual == expected)
-            for actual, expected in zip_longest(arrays_actual, arrays_expected)
-        ]
-        assert len(arrays_actual) == 3
-        assert all(comparisons)
-        assert steps_actual == steps_expected
+        assert steps_returned == steps_expected
 
-    def test_intermediary_every_third(self, numpy_array) -> None:
-        """Test `interval` parameter of 3."""
-        arrays_actual, steps_actual = zip(*chain_simulator(numpy_array, 5, 3))
-        arrays_expected, steps_expected = zip(
-            *(
-                (self.multiply_three, 3),
-                (self.multiply_five, 5),
-            )
+    @pytest.mark.parametrize(
+        "matrix, steps, interval, matrices_expected",
+        (
+            pytest.param(
+                initial_matrix, 10, 0, (multiply_nine,), id="interval=0"
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                1,
+                (
+                    multiply_zero,
+                    multiply_one,
+                    multiply_two,
+                    multiply_three,
+                    multiply_four,
+                    multiply_five,
+                    multiply_six,
+                    multiply_seven,
+                    multiply_eight,
+                    multiply_nine,
+                ),
+                id="interval=1",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                2,
+                (
+                    multiply_one,
+                    multiply_three,
+                    multiply_five,
+                    multiply_seven,
+                    multiply_nine,
+                ),
+                id="interval=2",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                3,
+                (
+                    multiply_two,
+                    multiply_five,
+                    multiply_eight,
+                    multiply_nine,
+                ),
+                id="interval=3",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                4,
+                (
+                    multiply_three,
+                    multiply_seven,
+                    multiply_nine,
+                ),
+                id="interval=4",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                5,
+                (
+                    multiply_four,
+                    multiply_nine,
+                ),
+                id="interval=5",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                6,
+                (
+                    multiply_five,
+                    multiply_nine,
+                ),
+                id="interval=6",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                7,
+                (
+                    multiply_six,
+                    multiply_nine,
+                ),
+                id="interval=7",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                8,
+                (
+                    multiply_seven,
+                    multiply_nine,
+                ),
+                id="interval=8",
+            ),
+            pytest.param(
+                initial_matrix,
+                10,
+                9,
+                (
+                    multiply_eight,
+                    multiply_nine,
+                ),
+                id="interval=9",
+            ),
+            pytest.param(
+                initial_matrix, 10, 10, (multiply_nine,), id="interval=10"
+            ),
+            pytest.param(
+                initial_matrix, 10, 11, (multiply_nine,), id="interval=11"
+            ),
+        ),
+    )
+    def test_intermediary_matrix(
+        self, matrix, steps, interval, matrices_expected
+    ):
+        """Test whether intermediary matrices are correct."""
+        matrices_returned, _ = zip(
+            *tuple(chain_simulator(matrix, steps, interval))
         )
-        comparisons = [
-            np.any(actual == expected)
-            for actual, expected in zip_longest(arrays_actual, arrays_expected)
-        ]
-        assert len(arrays_actual) == 2
-        assert all(comparisons)
-        assert steps_actual == steps_expected
+        combinations = zip_longest(matrices_returned, matrices_expected)
+        assert all(
+            np.all(returned == expected) for returned, expected in combinations
+        )
 
 
 class TestStateVectorProcessorNumPy:
