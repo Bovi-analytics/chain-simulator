dada8690f294ef6d50c25a730d651687b783f211
Maxximiser <mjh.nollet@student.han.nl>
2023-05-15 18:42:50 +0200

    Added lower-level vector processors to public API

--- src/chain_simulator/simulation.py
+++ src/chain_simulator/simulation.py
@@ -1,1 +1,13 @@
-from chain_simulator._simulation import state_vector_processor
+__all__ = [
+    "state_vector_processor",
+    "vector_processor_numpy",
+    "vector_processor_scipy",
+    "vector_processor_cupy",
+]
+
+from chain_simulator._simulation import (
+    state_vector_processor,
+    vector_processor_cupy,
+    vector_processor_numpy,
+    vector_processor_scipy,
+)
