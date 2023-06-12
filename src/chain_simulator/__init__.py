bd79770bec2727968e181151cb6b3a8cf5e10bb0
Maxximiser <mjh.nollet@student.han.nl>
2023-06-12 14:07:54 +0200

    Removed unnecessary __all__ attribute

--- src/chain_simulator/__init__.py
+++ src/chain_simulator/__init__.py
@@ -4,8 +4,6 @@ Generic, highly scalable platform for simulating digital twins using
 Markov chains.
 """
 
-__all__ = ["simulation", "utilities"]
-
 import logging
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
